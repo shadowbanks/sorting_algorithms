99ff6365e6f5d2a6b5e644ec4054ad8371531f92
Ambrose K <kolus7381@gmail.com>
2023-07-23 22:09:41 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *tmp;
 
-	if (*list == NULL || (*list)->next == NULL || list_len(*list) < 2)
+	if (list == NULL || *list == NULL || (*list)->next == NULL || list_len(*list) < 2)
 		return;
 
 	curr = (*list)->next;
