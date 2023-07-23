40f58dc547b9d24ded41aaefb32932ff499a21a0
Williams <akanniwilliams@gmail.com>
2023-07-23 10:40:04 +0100

    Correct the logic error in bubble sort function
    In the inner loop (the for loop) variable "i" was not initialized,
    this caused the inner loop to only execute during the first itration of the while loop.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,23 +10,24 @@ void bubble_sort(int *array, size_t size)
 {
 	int swap = 1, i = 1, len = 0, temp = 0;
 
-	if (array):
-		len = sizeof(array) / sizeof(array[0]);
+	if (array && size)
+		len = size;
 
 	while (swap)
 	{
-		swap = 0
-		for (; i < n; i++)
+		swap = 0;
+
+		for (i = 1; i < len; i++)
 		{
-			if (array[i] < array[i - 1])
+			if (array[i - 1] > array[i])
 			{
 				temp = array[i];
 				array[i] = array[i - 1];
 				array[i - 1] = temp;
 				swap = 1;
-				print_array(array);
+				print_array(array, size);
 			}
 		}
-		n--;
+		len--;
 	}
 }
--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,5 +24,7 @@ typedef struct listint_s *node;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /*SORT_H*/
